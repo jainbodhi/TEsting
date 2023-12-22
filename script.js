1bae6c87efcf629d7f0493a2876018a426d627f7
jainbodhi <99312614+jainbodhi@users.noreply.github.com>
2023-12-22 17:51:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 let root = ReactDOM.createRoot(document.getElementById("root"));
 let heading1 = React.createElement("h1", { id: "title1" }, "Harsh Jain");
-let heading2 = React.createElement("h1", { id: "title" }, "Jain Jain");
+let heading2 = React.createElement("h1", { id: "title2" }, "Jain Jain");
 let container = React.createElement(
   "div",
   { id: "contain" },
