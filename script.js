c3612591fe4a972b1da0e00dce79cdc28897e7e3
jainbodhi <99312614+jainbodhi@users.noreply.github.com>
2023-12-22 16:59:47 +0530

    Removed console.logs.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-console.log("Hello");
 let root = ReactDOM.createRoot(document.getElementById("root"));
 let heading1 = React.createElement("h1", { id: "title" }, "Harsh Jain");
 let heading2 = React.createElement("h1", { id: "title" }, "Jain Jain");
@@ -10,6 +9,4 @@ let container = React.createElement(
   heading1,
   heading2
 );
-console.log("Hello");
 root.render(container);
-console.log("Hello");
